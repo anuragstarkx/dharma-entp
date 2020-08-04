2a01420ed4e8c284b78e508babcf2e6d5e3bdc86
Anurag <anuragxprime@gmail.com>
2020-08-04 21:02:32 +0530

    Services section added

--- js/readJson.js
+++ js/readJson.js
@@ -7,7 +7,6 @@ function getYear() {
 function readJson() {
 	var blog_data = data;
 	var design_data = "";
-	console.log("here" + blog_data[0].title);
 	for (var i = blog_data.length - 1; i >= blog_data.length-3; i--) {
 		design_data += '<div class="card"><div class="card-body">' +
 						'<img class="card-img-top" src="' + blog_data[i].image + '" alt="Card image">' + 
@@ -24,7 +23,53 @@ function readJson() {
 	document.getElementById("blogit").appendChild(divtag);
 }
 
+function readCat() {
+	var cat = category;
+	var design_data = "";
+	for (var i = 0; i < cat.length; i++) {
+		design_data += '<div class="column"> \
+						<div class="card cat-card"> \
+							<img src="' + cat[i].img + '" alt="Category Image" class="card-img-top">\
+							<div class="card-body">\
+							<h4><b>' + cat[i].type + '</b></h4>\
+							<p>' + cat[i].desc + '</p>\
+							</div>\
+							<button class="btn btn-dark" onclick="viewProduct(' + cat[i].type + ')"><b>SHOW</b></button>\
+						</div>\
+					</div>';
+	}
+	var divtag =document.createElement('div');
+	divtag.className = "row";
+	divtag.innerHTML = design_data;
+	document.getElementById("cat").appendChild(divtag);
+}
+
 function start() {
 	getYear();
 	readJson();
+	readCat();
+}
+
+function moreBlog() {
+	var blog_data = data;
+	var design_data = "";
+	for (var i = blog_data.length - 1; i >= 0; i--) {
+		design_data += '<div class="card"><div class="card-body">' +
+						'<img class="card-img-top" src="' + blog_data[i].image + '" alt="Card image">' + 
+						'<div class="card-img-overlay">' + 
+						'<p class="card-text"><h2 class="text-center blog-card1 text-light"><b>' + blog_data[i].date + '</b></h2></p>' + 
+						'</div>' + 
+						'<h3 class="card-title">' + blog_data[i].title + '</h3>' + 
+						'<p class="card-text">' + blog_data[i].desc + '</p>' + 
+						'</div></div>'; 
+	}
+
+	var divtag = document.createElement('div');
+	divtag.className = "container flex-container";
+	divtag.innerHTML = design_data;
+	document.getElementById("blog-body").appendChild(divtag);
+}
+
+function emptyBlog() {
+	document.getElementById("blog-body").innerHTML = "";
 }
