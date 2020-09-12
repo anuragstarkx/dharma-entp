ca40b10e1c6cf67439658f835f72a6b762eeb249
Anurag <anuragxprime@gmail.com>
2020-09-13 01:43:54 +0530

    Updated few changes

--- js/readJson.js
+++ js/readJson.js
@@ -6,16 +6,23 @@ function getYear() {
 
 function readJson() {
 	var blog_data = data;
+	var count = 0;
 	var design_data = "";
-	for (var i = blog_data.length - 1; i >= blog_data.length-3; i--) {
-		design_data += '<div class="card"><div class="card-body">' +
-						'<img class="card-img-top" src="' + blog_data[i].image + '" alt="Card image">' + 
-						'<div class="card-img-overlay">' + 
-						'<p class="card-text"><h2 class="text-center blog-card text-light"><b>' + blog_data[i].date + '</b></h2></p>' + 
-						'</div>' + 
-						'<h3 class="card-title">' + blog_data[i].title + '</h3>' + 
-						'<p class="card-text">' + blog_data[i].desc + '</p>' + 
-						'</div></div>';
+	for (var i = blog_data.length - 1; i >= 0; i--) {
+		if(count<3){
+			design_data += '<div class="card"><div class="card-body">' +
+							'<img class="card-img-top" src="' + blog_data[i].image + '" alt="Card image">' + 
+							'<div class="card-img-overlay">' + 
+							'<p class="card-text"><h2 class="text-center blog-card text-light"><b>' + blog_data[i].date + '</b></h2></p>' + 
+							'</div>' + 
+							'<h3 class="card-title">' + blog_data[i].title + '</h3>' + 
+							'<p class="card-text">' + blog_data[i].desc + '</p>' + 
+							'</div></div>';
+						}
+						else{
+							break;
+						}
+		count++;
 	}
 	var divtag = document.createElement('div');
 	divtag.className = "card-deck";
@@ -49,14 +56,14 @@ function readClient() {
 	var client = clients;
 	var design_data = "";
 	for (var i = 0; i < client.length; i+=2) {
-		design_data += '<div class="col-lg-3 cli-col">\
+		design_data += '<div class="col-lg-3 cli-col" data-toggle="tooltip" data-placement="top" title="' + client[i].Detail + '">\
 						<img src="' + client[i].img + '" class="img-client">\
 						<p><h4><b>' + client[i].Client + '</b></h4></p>\
-					</div>\
-					<div class="col-lg-3 cli-col">\
-						<img src="' + client[i+1].img + '" class="img-client">\
-						<p><h4><b>' + client[i+1].Client + '</b></h4></p>\
-					</div>';
+						</div>\
+						<div class="col-lg-3 cli-col" data-toggle="tooltip" data-placement="top" title="' + client[i+1].Detail + '">\
+							<img src="' + client[i+1].img + '" class="img-client">\
+							<p><h4><b>' + client[i+1].Client + '</b></h4></p>\
+						</div>';
 	}
 	var divtag =document.createElement('div');
 	divtag.className = "row";
@@ -69,6 +76,9 @@ function start() {
 	readJson();
 	readCat();
 	readClient();
+	$(document).ready(function(){
+			$('[data-toggle="tooltip"]').tooltip();
+		});
 }
 
 function moreBlog() {
